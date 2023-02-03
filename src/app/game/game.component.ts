af0c71f45fc0abde5e3216eb47544e0262b2a281
Tomasz Smoczyński <tomeksmo@gmail.com>
2023-02-03 07:37:36 +0100

    pobranie gier

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  constructor(private authService: AuthService) { }
+  ngOnInit(): void {
+    console.log('START')
+
+    const a = this.authService.myGames().subscribe({
+      next: (data) => {
+        console.log(data)
+      },
+      error: (error) => {
+        if (error.status === 401) {
+          this.authService.loginRefresh().subscribe({
+            
+          })
+        }
+        console.log('taki bląd',error)
+      }
+    })
+    console.log('AAAAA', a)
+  }
 
 }
